cee7a50bba1e06e0834ffe730deab69a4841b220
Dorian1015 <lijinghailjh@163.com>
2021-06-01 23:41:29 +0800

    2021 6.1 病患资料表信息创建

---
+++ src/main/java/priv/ljh/uniapp/mapper/PatientDataMapper.java
@@ -0,0 +1,22 @@
+package priv.ljh.uniapp.mapper;
+
+import io.swagger.annotations.ApiModel;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+import priv.ljh.uniapp.entity.PatientData;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 病患资料表 Mapper 接口
+ * </p>
+ *
+ * @author lijinghai
+ * @since 2021-06-01
+ */
+@Mapper
+@Repository
+@ApiModel("病患资料表信息实体接口类")
+public interface PatientDataMapper extends BaseMapper<PatientData> {
+
+}
